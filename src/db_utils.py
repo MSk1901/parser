8940e10d01c0e71ddc1b89764394bb21e6b1248c
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2023-12-28 19:00:19 +0300

    add DBManager class

---
+++ src/db_utils.py
@@ -0,0 +1,19 @@
+from src.abs_classes import DataHandler
+
+
+class DBManager(DataHandler):
+
+    def get_companies_and_vacancies_count(self):
+        pass
+
+    def get_all_vacancies(self):
+        pass
+
+    def get_avg_salary(self):
+        pass
+
+    def get_vacancies_with_higher_salary(self):
+        pass
+
+    def get_vacancies_with_keyword(self, keyword: str):
+        pass
