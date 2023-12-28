668aad399c63aad86d2a9a5c6f880cf7895abcd3
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2023-12-28 18:58:22 +0300

    add DataHandler abstract class

--- src/abs_classes.py
+++ src/abs_classes.py
@@ -6,3 +6,26 @@ class JobsParser(ABC):
     @abstractmethod
     def get_vacancies(self):
         pass
+
+
+class DataHandler(ABC):
+
+    @abstractmethod
+    def get_companies_and_vacancies_count(self):
+        pass
+
+    @abstractmethod
+    def get_all_vacancies(self):
+        pass
+
+    @abstractmethod
+    def get_avg_salary(self):
+        pass
+
+    @abstractmethod
+    def get_vacancies_with_higher_salary(self):
+        pass
+
+    @abstractmethod
+    def get_vacancies_with_keyword(self, keyword: str):
+        pass
