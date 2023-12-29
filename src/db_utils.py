355f73023b3c6d078f8f29cc75a777c0e2245a27
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2023-12-29 13:05:00 +0300

    add PostgresExecutor class

--- src/db_utils.py
+++ src/db_utils.py
@@ -1,4 +1,25 @@
-from src.abs_classes import DataHandler
+import psycopg2
+
+from .abs_classes import DataHandler, DBExecutor
+
+
+class PostgresExecutor(DBExecutor):
+
+    def __init__(self, password):
+        self.__password = password
+
+    @property
+    def password(self):
+        return self.__password
+
+    def create_database(self, db_name: str):
+        pass
+
+    def create_table(self, table_name: str):
+        pass
+
+    def insert_values(self, values, table_name: str):
+        pass
 
 
 class DBManager(DataHandler):
