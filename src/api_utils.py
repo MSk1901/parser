a123326bf1ddb6f517de73b870008184e1ba41ea
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2023-12-29 20:59:27 +0300

    add get_employers func and refactor get_vacancies to include salary currency

--- src/api_utils.py
+++ src/api_utils.py
@@ -1,12 +1,34 @@
 import requests
 
-from src.abs_classes import JobsParser
+from .abs_classes import JobsParser
 
 
 class HeadHunterAPI(JobsParser):
 
     employers_id = ["15478", "9498120", "80", "4181", "1102601",
                     "3529", "78638", "1740", "3127", "2180"]
+
+    def get_employers(self):
+        """Осуществляет поиск информации о работодателе"""
+        employers_list = []
+        try:
+            for employer in self.employers_id:
+                # response = requests.get(f"https://api.hh.ru/employers/")
+                # data = response.json()
+
+                params = {"employer_id": employer, "only_with_salary": "true", "page": 0, "per_page": 1}
+                response = requests.get("https://api.hh.ru/vacancies", params=params)
+                data = response.json()["items"][0]
+
+                employer_data = {"id": data["employer"]["id"],
+                                 "name": data["employer"]["name"],
+                                 "url": data["employer"]["alternate_url"]
+                                 }
+                employers_list.append(employer_data)
+            return employers_list
+
+        except (requests.exceptions.HTTPError, ValueError, KeyError) as e:
+            raise Exception(e)
 
     def get_vacancies(self):
         """Осуществляет поиск вакансий по ключевому слову и сохраняет вакансии в список"""
@@ -25,7 +47,7 @@ class HeadHunterAPI(JobsParser):
 
                     salary_to = vacancy["salary"]["to"]
                     if not salary_to:
-                        salary_to = 0
+                        salary_to = salary_from
 
                     vacancy_dict = {"id": vacancy["id"],
                                     "name": vacancy["name"],
@@ -35,6 +57,7 @@ class HeadHunterAPI(JobsParser):
                                     "area": vacancy["area"]["name"],
                                     "salary_from": salary_from,
                                     "salary_to": salary_to,
+                                    "salary_currency": vacancy["salary"]["currency"],
                                     "schedule": vacancy["schedule"]["name"],
                                     "employment_type": vacancy["employment"]["name"],
                                     "requirements": vacancy["snippet"]["requirement"]}
